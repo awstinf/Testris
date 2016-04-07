9f62483df1ca052d5f8549b06bb4278c9e497e03
AustinFoley96 <austinfoley96@gmail.com>
2016-04-07 20:48:33 +0100

    Initial Commit

---
+++ src/testris/Testris.java
@@ -0,0 +1,16 @@
+package testris;
+
+/**
+ *
+ * @author Austin
+ */
+public class Testris {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
